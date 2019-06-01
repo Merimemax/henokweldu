99f8140b13d6d1f00d226cd1cff70872aa46bcc3
Merih <merimemax@gmail.com>
2019-05-31 20:01:49 -0400

    updates on henoks review

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { PublicationacceptedComponent } from './publicationaccepted/publicationaccepted.component';
-import { PublicationunderreviewComponent } from './publicationunderreview/publicationunderreview.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ConferenceabstractComponent } from './conferenceabstract/conferenceabstract.component';
 import { PublicationComponent } from './publication/publication.component';
@@ -13,10 +10,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'resume', component: ResumeComponent },
   { path: 'publications', component: PublicationComponent },
-  { path: 'publication-accepted', component: PublicationacceptedComponent },
-  { path: 'publication-under-review', component: PublicationunderreviewComponent },
   { path: 'conference-abstract', component: ConferenceabstractComponent },
-  { path: 'contact', component: ContactComponent }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { PublicationacceptedComponent } from './publicationaccepted/publicationaccepted.component';
-import { PublicationunderreviewComponent } from './publicationunderreview/publicationunderreview.component';
 import { ResumeComponent } from './resume/resume.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -25,9 +22,6 @@ import { PublicationComponent } from './publication/publication.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    ContactComponent,
-    PublicationacceptedComponent,
-    PublicationunderreviewComponent,
     ResumeComponent,
     NavbarComponent,
     ConferenceabstractComponent,
